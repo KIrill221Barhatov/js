587fa62e762340ac5e296b56f99dba9bd41b8c28
Кирилл Бархатов <117072370+KIrill221Barhatov@users.noreply.github.com>
2023-02-06 19:10:05 +0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,98 @@
+// 241.1
+// let button = document.querySelector('#elem1')
+// console.log(button)
+// let button1 = document.querySelector('#elem2')
+// console.log(button1)
+// let button2 = document.querySelector('#elem3')
+// console.log(button2)
+// 241.2
+// let button = document.querySelector('#block')
+// console.log(button)
+// 241.3
+// let button = document.querySelector('#block1')
+// console.log(button)
+// 241.4
+// let button = document.querySelector('#www')
+// console.log(button)
+// 242
+// let button = document.querySelector('#button1')
+// button.addEventListener('click', function() {
+//     console.log('1')
+// })
+
+// let button1 = document.querySelector('#button2')
+// button1.addEventListener('click', function() {
+//     console.log('2')
+// })
+// let button2 = document.querySelector('#button3')
+// button2.addEventListener('click', function() {
+//     console.log('3')
+// })
+// 243
+// let button1 = document.querySelector('#button1')
+// button1.addEventListener('click', func1)
+// function func1() {
+// 	console.log(1);
+// }
+// let button2 = document.querySelector('#button2')
+// button2.addEventListener('click', func2)
+// function func2() {
+// 	console.log(2);
+// }
+
+// 244
+// let button1 = document.querySelector('#elem1')
+// let button2 = document.querySelector('#elem2')
+// let button3 = document.querySelector('#elem3')
+// let button4 = document.querySelector('#elem4')
+// let button5 = document.querySelector('#elem5')
+// button1.addEventListener('click', func)
+// button2.addEventListener('click', func)
+// button3.addEventListener('click', func)
+// button4.addEventListener('click', func)
+// button5.addEventListener('click', func)
+// function func(){
+//     console.log('message')
+// }
+// 245
+// let button = document.querySelector('#elem')
+// button.addEventListener('click', func1)
+// button.addEventListener('click', func2)
+// button.addEventListener('click', func3)
+// function func1() {
+// 	console.log('1');
+// }
+
+// function func2() {
+// 	console.log('2');
+// }
+
+// function func3() {
+// 	console.log('3');
+// }
+// 246.1
+// let button = document.querySelector('#elem');
+// button.addEventListener('dblclick', function() {
+//     console.log('1')
+// })
+// 246.2
+// let button = document.querySelector('#elem');
+// button.addEventListener('mouseover', function() {
+//     console.log('2')
+// 
+// })
+// 246.3
+// let button = document.querySelector('#elem');
+// button.addEventListener('mouseout', function() {
+//     console.log('3')
+//
+// })
+// 246.4
+// let button = document.querySelector('#elem');
+// button.addEventListener('mouseover', function() {
+//     console.log('4')
+// })
+// let button = document.querySelector('#elem');
+// button.addEventListener('mouseout', function() {
+//     console.log('5')
+// })
